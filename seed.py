4379cb9dd9ec473ff03b3f45cb9d3137a404548f
EVYN ROSE <evynrosebud@gmail.com>
2024-04-17 16:44:47 +0100

    added syeds lists to our db

--- seed.py
+++ seed.py
@@ -48,6 +48,33 @@ with app.app_context():
         )
         MultipleSclerosis.save()
 
+        Depression = ConditionsModel(
+            name="Depression",
+            about="I have dealt with depression on and off since 2019.",
+            info="Depression is a mental health condition, that, depending on the certain person and that person's mental state, can make it difficult to live their everyday life",
+            advice="Make self care a priority for yourself everyday, and remember you don’t deserve to feel like this.",
+            user_id=user.id,
+        )
+        Depression.save()
+
+        Anxiety = ConditionsModel(
+            name="Anxiety",
+            about="My name is Greg and I have had anxiety since 2012. It takes a toll on me daily.",
+            info="Anxiety is a mental health condition characterized by excessive worry, fear, and nervousness that can significantly impact daily life and well-being.",
+            advice="See your therapist and be proactive about not missing any appointments.",
+            user_id=user.id,
+        )
+        Anxiety.save()
+
+        ObsessiveCompulsiveDisorder = ConditionsModel(
+            name="OCD, Obsessive Compulsive Disorder",
+            about="My name is Dale and I have had OCD since I was a child.",
+            info="OCD, or Obsessive-Compulsive Disorder, is a mental health condition characterized by intrusive, unwanted thoughts (obsessions) and repetitive behaviors or rituals (compulsions) aimed at reducing distress or preventing perceived harm.",
+            advice="It's okay to not be okay.",
+            user_id=user.id,
+        )
+        ObsessiveCompulsiveDisorder.save()
+
         comment1 = CommentModel(
             content="I have Crohns, too! It really affects my skin and eyes.",
             title="Crohn's Sisters!",
@@ -68,6 +95,27 @@ with app.app_context():
             conditions_id=MultipleSclerosis.id,
         )
         comment3.save()
+
+        comment4 = CommentModel(
+            content="I have depression as well. Medication helps me deal with it along with therapy.",
+            title="Depression struggles",
+            conditions_id=Depression.id,
+        )
+        comment4.save()
+
+        comment5 = CommentModel(
+            content="Please know you aren't alone in this!",
+            title="Anxiety is a rough thing to deal with.",
+            conditions_id=Anxiety.id,
+        )
+        comment5.save()
+
+        comment6 = CommentModel(
+            content="I wish my compulsions weren't so hard to deal with!",
+            title="OCD is a daily battle.",
+            conditions_id=Anxiety.id,
+        )
+        comment6.save()
 
         print("Database seeded!")
     except Exception as e:
